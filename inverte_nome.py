6acf255e7fcad7393a6ec012bbef05c0010aaa19
Rafael Puyau <rafael.puyau@gmail.com>
2020-11-22 00:40:17 -0300

    Simplesmente, acrescentei um comentário antes do slice dentro do print

--- inverte_nome.py
+++ inverte_nome.py
@@ -3,4 +3,7 @@ Script simples para inverter uma string
 '''
 nome = input('Qual seu nome? ')
 
+# Uma string é uma cadeia de caracteres, assim
+# basta usar o slice de string lendo a mesma de
+# trás pra frente
 print(nome[::-1].lower())
