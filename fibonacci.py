7883b96f80c92603b4f57e4b884313becc6bd88b
Rafael Puyau <rafael.puyau@gmail.com>
2020-11-22 20:42:44 -0300

    Série de Fibonacci. Teste de execução de performance

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,12 @@
-#!/usr/bin env python
+'''
+Série de fibonacci em Python para testar o tempo de execução
+'''
 fibonacci = 1
 last1 = 0
 last2 = 1
 print('Starting...')
 print(f'{last1}')
 for i in range(1_000_000):
-    # print(f'{fibonacci} => ', end=' ')
     print(f'{fibonacci}')
     fibonacci = last1 + last2
     last1 = last2
