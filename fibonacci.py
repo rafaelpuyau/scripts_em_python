d08b20c268bcbc56e6d15455d342eb01c3fff6fa
Rafael Puyau <rafael.puyau@gmail.com>
2020-11-22 20:41:15 -0300

    Quão veloz e eficiente é python? Um teste de tempo de execução!

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+#!/usr/bin env python
+fibonacci = 1
+last1 = 0
+last2 = 1
+print('Starting...')
+print(f'{last1}')
+for i in range(1_000_000):
+    # print(f'{fibonacci} => ', end=' ')
+    print(f'{fibonacci}')
+    fibonacci = last1 + last2
+    last1 = last2
+    last2 = fibonacci
+print('The End')
